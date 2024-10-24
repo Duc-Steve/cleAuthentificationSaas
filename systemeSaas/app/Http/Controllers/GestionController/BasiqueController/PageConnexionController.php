171c77efaa661850f688d205b8f529d7107bfdf5
Duc-Steve <stevelioni600@gmail.com>
2024-10-24 13:58:27 +0000

    Page connexion

--- systemeSaas/app/Http/Controllers/GestionController/BasiqueController/PageConnexionController.php
+++ systemeSaas/app/Http/Controllers/GestionController/BasiqueController/PageConnexionController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\GestionController\BasiqueController;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class PageConnexionController extends Controller
 {
     //
+    
+    public function index(): RedirectResponse|View
+    {   
+        return view('basiques.connexion');
+    }
 }
